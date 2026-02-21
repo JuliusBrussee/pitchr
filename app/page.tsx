99c1fbc3f8a8896fc7814c39bf910d8f83fed3ec
Arav <arav.madan2205@gmail.com>
2026-02-21 15:57:55 +0100

    chore: commit remaining project updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import HeadCoachSandbox from "../views/components/HeadCoachSandbox.jsx";
+
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Pitchr</h1>
-    </main>
-  );
+  return <HeadCoachSandbox />;
 }
